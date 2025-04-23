b6da0fa7fcadf7e1f2c5e81a6fd362d545604f70
zachsusee <susee@wustl.edu>
2025-04-23 22:55:27 +0200

    migrated

--- takehomebelong/events/models.py
+++ takehomebelong/events/models.py
@@ -6,7 +6,7 @@ class Event(models.Model):
     image = models.URLField()
     title = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     location = models.CharField(max_length=200)
     start_date = models.DateField()
     start_time = models.TimeField()
