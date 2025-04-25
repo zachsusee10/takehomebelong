c071e143485bc7f0d6ce7a087d5b497e3911f682
zachsusee <susee@wustl.edu>
2025-04-25 11:03:47 +0200

    basic setup done

--- takehomebelong/events/views.py
+++ takehomebelong/events/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Event
 # Create your views here.
+
+
+
+def catalog_view(request):
+    events = Event.objects.all()
+    return render(request, 'catalog.html', {'events': events})
+
+def event_detail(request, event_id):
+    event = request.GET.get(Event, id=event_id)
+    return render(request, 'events/events.html', {
+        'event': event,
+    })
