b43a4d24a175b712a1a3098f7a2f7c58e273bb69
zachsusee <susee@wustl.edu>
2025-04-25 12:08:18 +0200

    added basic functions, cart html

--- takehomebelong/events/views.py
+++ takehomebelong/events/views.py
@@ -9,7 +9,17 @@ def catalog_view(request):
     return render(request, 'catalog.html', {'events': events})
 
 def event_detail(request, event_id):
-    event = request.GET.get(Event, id=event_id)
-    return render(request, 'events/events.html', {
-        'event': event,
-    })
+    event = get_object_or_404(Event, id=event_id)
+    return render(request, 'events.html', {'event': event})
+
+def add_to_cart(request, event_id):
+    cart = request.session.get('cart', [])
+    if event_id not in cart:
+        cart.append(event_id)
+        request.session['cart'] = cart
+    return redirect('catalog.html')
+
+def cart_view(request):
+    cart = request.session.get('cart', [])
+    cart_events = Event.objects.filter(id__in=cart)
+    return render(request, 'cart.html', {'events': cart_events})
